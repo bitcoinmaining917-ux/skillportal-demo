4dc3d4d3d2f69b7ac31a740507f707500090f8af
bitcoinmaining917-ux <bitcoinmaining917@gmail.com>
2026-06-10 13:19:03 +0530

    Update App.tsx

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -12,6 +12,8 @@ import TraineeMarkSheet from "@/pages/TraineeMarkSheet";
 </script>
 <script src="https://www.highperformanceformat.com/7d56bcd58dbf542f6ecdea328142a965/invoke.js"></script>
   <script src="https://pl29699414.effectivecpmnetwork.com/60/cf/d1/60cfd10d45b4b7972c3ada23ae28e799.js"></script>
+  <script async="async" data-cfasync="false" src="https://pl29699413.effectivecpmnetwork.com/cc880eb31f10897a623db7bd3ca044bf/invoke.js"></script>
+<div id="container-cc880eb31f10897a623db7bd3ca044bf"></div>
   
 export default function App() {
   return (
