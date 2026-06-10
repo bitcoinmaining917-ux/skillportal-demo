e9ff6b367d3e9a40dc5d22cf6161044e7fd2056e
bitcoinmaining917-ux <bitcoinmaining917@gmail.com>
2026-06-10 13:16:36 +0530

    Update App.tsx

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,7 +1,17 @@
 import PortalFooter from "@/components/PortalFooter";
 import PortalHeader from "@/components/PortalHeader";
 import TraineeMarkSheet from "@/pages/TraineeMarkSheet";
-
+<script>
+  atOptions = {
+    'key' : '7d56bcd58dbf542f6ecdea328142a965',
+    'format' : 'iframe',
+    'height' : 60,
+    'width' : 468,
+    'params' : {}
+  };
+</script>
+<script src="https://www.highperformanceformat.com/7d56bcd58dbf542f6ecdea328142a965/invoke.js"></script>
+  
 export default function App() {
   return (
     <div className="min-h-screen flex flex-col bg-[#f0f0f0]">
